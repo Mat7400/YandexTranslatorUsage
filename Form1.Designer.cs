b951473adee8dd0629acf9e78bb2f1f222daf297
Mat7400 <mmatuha74@gmail.com>
2022-04-30 12:26:40 +0300

    yandex translator

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace UseLibrary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 89);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get KURS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "USD",
+            "EUR"});
+            this.comboBox1.Location = new System.Drawing.Point(312, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(268, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(338, 159);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 152);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Translate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Yandex.Cloud;
+using Yandex.Cloud.Credentials;
+
+namespace UseLibrary
+{
+    public partial class Form1 : Form
+    {
+        CalcValut.calcValut calc = new CalcValut.calcValut();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //используем калькулятор из библиотеки написанной "дргуим программистом"
+            double res = calc.kalkulateKurs(100,CalcValut.valut.USD);
+            MessageBox.Show(res.ToString());
+        }
+        YandexTranslator tr;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tr = new YandexTranslator();
+            foreach (var item in tr.langs())
+            {
+                comboBox1.Items.Add(item);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            string langfrom = "ru";
+            string langto = comboBox1.SelectedItem.ToString();
+            var res = tr.translate(langfrom, langto, text);
+            MessageBox.Show(res);
+        }
+    }
+    class YandexTranslator
+    {
+        private Sdk yasdk;
+        private string Oauth = "";
+        private string folderID = "";
+        public YandexTranslator()
+        {
+            yasdk = new Sdk(new OAuthCredentialsProvider(Oauth));
+        }
+        public string translate(string langfrom, string langto, params string[] texts)
+        {
+            //yandex translation example
+
+            var req = new Yandex.Cloud.Ai.Translate.V2.TranslateRequest();
+            req.FolderId = folderID;
+            req.TargetLanguageCode = langto;
+            req.SourceLanguageCode = langfrom;
+            foreach (var text in texts)
+            {
+                req.Texts.Add(text);
+            }
+
+            var ans = yasdk.Services.Ai.Translate.TranslationService.Translate(req);
+            string res = String.Join(" ", ans.Translations);
+
+            return res;
+        }
+        public List<string> langs()
+        {
+            var req = new Yandex.Cloud.Ai.Translate.V2.ListLanguagesRequest();
+            req.FolderId = folderID;
+            var available = yasdk.Services.Ai.Translate.TranslationService.ListLanguages(req);
+            List<string> res = new List<string>();
+            foreach (var lang in available.Languages)
+            {
+                res.Add(lang.Code);
+            }
+            return res;
+        }
+    }
+}
